408d476c2e79f564281cd8ff0c9e89bf5182860a
Gwanho Kim <83526533+khkim6040@users.noreply.github.com>
2023-10-03 01:22:04 +0900

    [Silver III] Title: 이번에는 C번이 문자열, Time: 4 ms, Memory: 2464 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/29198. 이번에는 C번이 문자열/이번에는 C번이 문자열.cc
+++ 백준/Silver/29198. 이번에는 C번이 문자열/이번에는 C번이 문자열.cc
@@ -1,48 +1,38 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <queue>
 #include <string>
 using namespace std;
 
 int N, M, K;
-struct Input {
-   string str;
-   int size;
-   bool operator<(const Input& other) const {
-      return size < other.size;
-   }
-};
-priority_queue<Input> pq;
+priority_queue<string> pq;
 
 int main() {
-   ios_base::sync_with_stdio(false);
-   cin.tie(NULL);
-   cin >> N >> M >> K;
-   for (int i = 0; i < N; i++) {
-      // Get S and calculate its size
-      int sum = 0;
-      string str;
-      cin >> str;
-      for (char c : str) {
-         sum += int(c);
-      }
-      // Use priority queue
-      if (pq.size() < K) {
-         pq.push({str, sum});
-      } else if (pq.size() == K && pq.top().size > sum) {
-         pq.pop();
-         pq.push({str, sum});
-      }
-   }
-   // Assemble all queue elements
-   string answer;
-   while (!pq.empty()) {
-      string str = pq.top().str;
-      pq.pop();
-      answer += str;
-   }
-   // Sort ascending order
-   sort(answer.begin(), answer.end());
-   cout << answer;
-   return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> N >> M >> K;
+    for (int i = 0; i < N; i++) {
+        // Get S and sort ascending order
+        string str;
+        cin >> str;
+        sort(str.begin(), str.end());
+        // Use priority queue
+        if (pq.size() < K) {
+            pq.push(str);
+        } else if (pq.size() == K && pq.top() > str) {
+            pq.pop();
+            pq.push(str);
+        }
+    }
+    // Assemble queue elements
+    string answer;
+    while (!pq.empty()) {
+        string str = pq.top();
+        pq.pop();
+        answer += str;
+    }
+    // Sort ascending order
+    sort(answer.begin(), answer.end());
+    cout << answer;
+    return 0;
 }
