70d78a2dc87da6b6bc57f4a101c115d72329cb8c
Gwanho Kim <83526533+khkim6040@users.noreply.github.com>
2024-07-18 12:48:26 +0900

    [Gold II] Title: 게임, Time: 0 ms, Memory: 1248 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.cc
+++ 백준/Gold/1103. 게임/게임.cc
@@ -1,72 +1,75 @@
-#include <iostream>
-#include <string>
-using namespace std;
-typedef long long ll;
-#define pii pair<ll, int>
+#include <stdio.h>
 
-int N, M;
-int b[55][55];
-int cnt[55][55];
-int dr[] = {-1, 1, 0, 0};
-int dc[] = {0, 0, -1, 1};
-bool visited[55][55];
-bool is_cycle;
+int IsCycle;
+int Sero, Garo;
 
-int dfs(int r, int c) {
-    if (cnt[r][c]) {
-        return cnt[r][c];
-    }
+char Map[55][55];
+char InputMap[55][55];
 
-    bool flag = false;
-    for (int i = 0; i < 4; i++) {
-        int nr = r + dr[i] * b[r][c];
-        int nc = c + dc[i] * b[r][c];
-        if (nr >= 0 && nc >= 0 && nr < N && nc < M && b[nr][nc] != 24) {
-            flag = true;
-            if (visited[nr][nc]) {
-                is_cycle = true;
-                return 0;
-            }
-            visited[nr][nc] = true;
-            cnt[r][c] = max(cnt[r][c], dfs(nr, nc) + 1);
-            visited[nr][nc] = false;
-        }
-    }
+int Memo[51][51];
+int IsVisit[51][51];
 
-    if (!flag) {
-        cnt[r][c] = 1;
-    }
+int dS[4] = {1, -1, 0, 0};
+int dG[4] = {0, 0, 1, -1};
 
-    return cnt[r][c];
+int Max(int num1, int num2) {
+    if (num1 > num2)
+        return num1;
+    else
+        return num2;
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+void Input() {
+    scanf("%d %d", &Sero, &Garo);
 
-    cin >> N >> M;
-    for (int i = 0; i < N; i++) {
-        string s;
-        cin >> s;
-        for (int j = 0; j < M; j++) {
-            b[i][j] = s[j] - '0';  // 'H' - '0' == 24
+    for (int idx = 0; idx < Sero; idx++)
+        scanf("%s", InputMap[idx]);
+
+    for (int idx = 0; idx < Sero; idx++)
+        for (int jdx = 0; jdx < Garo; jdx++)
+            Map[idx + 1][jdx + 1] = InputMap[idx][jdx];
+}
+
+int DFS(int NowS, int NowG) {
+    if (Memo[NowS][NowG])
+        return Memo[NowS][NowG];
+
+    bool flag = false;
+    for (int idx = 0; idx < 4; idx++) {
+        int NextS = NowS + (Map[NowS][NowG] - '0') * dS[idx];
+        int NextG = NowG + (Map[NowS][NowG] - '0') * dG[idx];
+
+        if (NextS >= 1 && NextG >= 1 && NextS <= Sero && NextG <= Garo) {
+            if ('1' <= Map[NextS][NextG] && Map[NextS][NextG] <= '9') {
+                flag = true;
+                if (IsVisit[NextS][NextG] == 1)
+                    IsCycle = 1;
+                else {
+                    IsVisit[NextS][NextG] = 1;
+                    Memo[NowS][NowG] = Max(Memo[NowS][NowG], DFS(NextS, NextG) + 1);
+                    IsVisit[NextS][NextG] = 0;
+                }
+            }
         }
     }
 
-    visited[0][0] = true;
-    dfs(0, 0);
+    if (!flag) Memo[NowS][NowG] = 1;
+    return Memo[NowS][NowG];
+}
 
-    // for (int i = 0; i < N; i++) {
-    //     for (int j = 0; j < M; j++) {
-    //         cout << cnt[i][j] << ' ';
-    //     }
-    //     cout << '\n';
-    // }
+void Solve() {
+    Input();
 
-    if (is_cycle) {
-        cout << -1;
-    } else {
-        cout << cnt[0][0];
-    }
+    IsVisit[1][1] = 1;
+    int Answer = DFS(1, 1);
+
+    if (IsCycle == 1)
+        printf("-1\n");
+    else
+        printf("%d\n", Answer);
+}
+
+int main(void) {
+    Solve();
     return 0;
 }
