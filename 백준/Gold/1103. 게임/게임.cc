2dca8b5d37ce16dc7a81f056914e12922eae8e57
Gwanho Kim <83526533+khkim6040@users.noreply.github.com>
2024-07-18 12:55:24 +0900

    [Gold II] Title: 게임, Time: 0 ms, Memory: 1212 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.cc
+++ 백준/Gold/1103. 게임/게임.cc
@@ -34,7 +34,10 @@ int DFS(int NowS, int NowG) {
     if (Memo[NowS][NowG])
         return Memo[NowS][NowG];
 
+    IsVisit[NowS][NowG] = 1;
+
     bool flag = false;
+
     for (int idx = 0; idx < 4; idx++) {
         int NextS = NowS + (Map[NowS][NowG] - '0') * dS[idx];
         int NextG = NowG + (Map[NowS][NowG] - '0') * dG[idx];
@@ -42,31 +45,37 @@ int DFS(int NowS, int NowG) {
         if (NextS >= 1 && NextG >= 1 && NextS <= Sero && NextG <= Garo) {
             if ('1' <= Map[NextS][NextG] && Map[NextS][NextG] <= '9') {
                 flag = true;
+
                 if (IsVisit[NextS][NextG] == 1)
                     IsCycle = 1;
                 else {
-                    IsVisit[NextS][NextG] = 1;
                     Memo[NowS][NowG] = Max(Memo[NowS][NowG], DFS(NextS, NextG) + 1);
-                    IsVisit[NextS][NextG] = 0;
                 }
             }
         }
     }
 
     if (!flag) Memo[NowS][NowG] = 1;
+    IsVisit[NowS][NowG] = 0;
     return Memo[NowS][NowG];
 }
 
 void Solve() {
     Input();
 
-    IsVisit[1][1] = 1;
     int Answer = DFS(1, 1);
 
     if (IsCycle == 1)
         printf("-1\n");
     else
         printf("%d\n", Answer);
+
+    // for (int i = 1; i <= Sero; i++) {
+    //     for (int j = 1; j <= Garo; j++) {
+    //         printf("%d", Memo[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 }
 
 int main(void) {
