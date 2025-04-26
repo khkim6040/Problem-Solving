4bbe0ebd3f8702d68b42b924b1f99de2112050ef
Gwanho Kim <khkim6040@gmail.com>
2025-04-26 14:37:41 +0900

    [Gold I] Title: 그림 교환, Time: 12 ms, Memory: 6116 KB -BaekjoonHub

--- 백준/Gold/1029. 그림 교환/그림 교환.cc
+++ 백준/Gold/1029. 그림 교환/그림 교환.cc
@@ -5,6 +5,7 @@ using namespace std;
 int N;
 int buy[16][16];
 int dp[1<<16][16];
+int ans = 1;
 
 int dfs(int status, int cur, int cost) {
     int cur_status = status | (1<<cur);
@@ -14,14 +15,13 @@ int dfs(int status, int cur, int cost) {
 
     int len = 0;
     for(int i=1; i<N; i++) {
-        if(cost <= buy[cur][i] && (cur_status & (1<<i)) == 0) {
-            dp[cur_status][cur] = max(dp[cur_status][cur], dfs(cur_status, i, buy[cur][i])+1); 
-        }
+        if(cost > buy[cur][i] || (cur_status & (1<<i)) || i == cur) continue;
+        len = max(len, dfs(cur_status | (1<<cur), i, buy[cur][i])+1); 
     }
 
     // cout << cur_status << ' ' << cur << ' ' << cost << ' ' << len << '\n';
 
-    return dp[cur_status][cur];
+    return dp[cur_status][cur] = len;
 }
 
 int main() {
