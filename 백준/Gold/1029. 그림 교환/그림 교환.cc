0fcfed5956afdb45e0759d6088e8e2e8c09459a2
Gwanho Kim <khkim6040@gmail.com>
2025-04-26 14:36:31 +0900

    [Gold I] Title: 그림 교환, Time: 12 ms, Memory: 6116 KB -BaekjoonHub

---
+++ 백준/Gold/1029. 그림 교환/그림 교환.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int buy[16][16];
+int dp[1<<16][16];
+
+int dfs(int status, int cur, int cost) {
+    int cur_status = status | (1<<cur);
+    if(dp[cur_status][cur]) {
+        return dp[cur_status][cur];
+    }
+
+    int len = 0;
+    for(int i=1; i<N; i++) {
+        if(cost <= buy[cur][i] && (cur_status & (1<<i)) == 0) {
+            dp[cur_status][cur] = max(dp[cur_status][cur], dfs(cur_status, i, buy[cur][i])+1); 
+        }
+    }
+
+    // cout << cur_status << ' ' << cur << ' ' << cost << ' ' << len << '\n';
+
+    return dp[cur_status][cur];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    cin >> N;
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N; j++) {
+            char c;
+            cin >> c;
+            buy[i][j] = c - '0';
+        }
+    }
+
+    
+
+    cout << dfs(0, 0, 0)+1;
+
+    return 0;
+}
+
+// 32 16 8 4 2 1
