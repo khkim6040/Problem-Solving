02daefe216be7be80135abdb38fb1bc5d15630a9
Gwanho Kim <khkim6040@gmail.com>
2025-11-04 17:04:24 +0900

    [level 2] Title: 완전범죄, Time: 0.03 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
@@ -7,26 +7,26 @@ using namespace std;
 
 int N;
 
-bool cmp(vector<int> a, vector<int> b) {
-    return float(a[0])/a[1] > float(b[0])/b[1]; // 값 범위 1~3이므로 값마다 나오는 수 조합 유일함
-}
+int dp[44][122];
 
 int solution(vector<vector<int>> info, int n, int m) {
-    int prevN = n;
     N = info.size();
     
-    sort(info.begin(), info.end(), cmp);
-    
-    for(int i=0; i<N; i++) {
-        // cout << info[i][0] << ' ' << info[i][1] << '\n';
-        if(m > info[i][1]) {
-            m -= info[i][1];
-        } else if(n > info[i][0]) {
-            n -= info[i][0];
-        } else {
-            return -1;
+    int sumA = 0;
+    for(int i=1; i<=N; i++) {
+        int a = info[i-1][0];
+        sumA += a;
+        int b = info[i-1][1];
+        for(int j=0; j<=m; j++) {
+            dp[i][j] = dp[i-1][j];
+            if(j>b) {
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-b]+a);
+            }
+            // cout << dp[i][j] << ' ';
         }
+        // cout << '\n';
     }
     
-    return prevN-n;
+    int answer = sumA - dp[N][m];
+    return answer >= n ? -1 : answer;
 }
