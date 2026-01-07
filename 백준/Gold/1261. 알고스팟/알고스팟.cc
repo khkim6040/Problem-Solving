727a17e8840da10254346646354a3ce197eb35a8
Gwanho Kim <khkim6040@gmail.com>
2026-01-07 14:20:37 +0900

    [Gold IV] Title: 알고스팟, Time: 0 ms, Memory: 2420 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.cc
+++ 백준/Gold/1261. 알고스팟/알고스팟.cc
@@ -8,49 +8,19 @@ using namespace std;
 
 struct Node {
     int r, c, cnt;
+
+    bool operator< (const Node &other) const {
+        return cnt > other.cnt;
+    }
 };
 
-int N, M;
+int N, M, cnt;
 int b[105][105];
-bool visited[105][105][10005];
+int dist[105][105];
 
 int dr[] = {-1, 1, 0, 0};
 int dc[] = {0, 0, -1, 1};
 
-bool check(int limit) {
-    queue<Node> q;
-    q.push({1, 1, 0});
-
-    fill_n(&visited[0][0][0], 105*105*10005, false);
-    visited[1][1][0] = true;
-
-    while(!q.empty()) {
-        Node cur = q.front();
-        q.pop();
-
-        if(cur.r == N && cur.c == M) {
-            return true;
-        }
-
-        for(int i=0; i<4; i++) {
-            int r = cur.r + dr[i];
-            int c = cur.c + dc[i];
-            if(r<=0 || r>N || c<=0 || c>M) continue;
-            if(b[r][c] == 1) {
-                if(cur.cnt == limit) continue;
-                if(visited[r][c][cur.cnt+1]) continue;
-                visited[r][c][cur.cnt+1] = true;
-                q.push({r, c, cur.cnt+1});
-            } else {
-                if(visited[r][c][cur.cnt]) continue;
-                visited[r][c][cur.cnt] = true;
-                q.push({r, c, cur.cnt});
-            }
-        }
-    }
-
-    return false;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -58,32 +28,40 @@ int main() {
 
     cin >> M >> N;
 
-    int cnt = 0;
     for(int i=1; i<=N; i++) {
         for(int j=1; j<=M; j++) {
             char c;
             cin >> c;
             b[i][j] = c-'0';
             if(b[i][j]==1) cnt++;
+            dist[i][j] = 2111111111;
         }
     }
 
-    int l = 0;
-    int r = cnt;
+    priority_queue<Node> q;
+    q.push({1, 1, 0});
+    dist[1][1] = 0;
 
-    while(l<=r) {
-        int mid = (l+r)/2;
-        // cout << l << ' ' << r << '\n';
-        if(check(mid)) {
-            // cout << "T\n";
-            r = mid-1;
-        } else {
-            // cout << "F\n";
-            l = mid+1;
+    while(!q.empty()) {
+        Node cur = q.top();
+        q.pop();
+
+        if(cur.r == N && cur.c == M) break;
+
+        for(int i=0; i<4; i++) {
+            int r = cur.r + dr[i];
+            int c = cur.c + dc[i];
+            if(r<=0 || r>N || c<=0 || c>M) continue;
+            int d = cur.cnt;
+            if(b[r][c]==1) d++;
+            if(d < dist[r][c]) {
+                dist[r][c] = d;
+                q.push({r, c, d});
+            }
         }
     }
 
-    cout << r+1;
-
+    cout << dist[N][M];
+    
     return 0;
 }
